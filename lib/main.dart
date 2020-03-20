61664584c49321f2e6b5077bf6559c20c2b8a6af
Sui <rainjie87@gmail.com>
2020-03-20 11:19:05 +0100

    My first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home() ,
+));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var growFlowerLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Yujie ID card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            growFlowerLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/orchid.jpg'),
+                radius: 40,
+
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.amberAccent,
+            ),
+
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                fontFamily: 'IndieFlower',
+                letterSpacing: 2.0,
+                ),
+
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Yujie Sui',
+              style: TextStyle(
+                color:Colors.amberAccent[200],
+                fontFamily: 'IndieFlower',
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'GROWFlowers LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                fontFamily: 'IndieFlower',
+                letterSpacing: 2.0,
+              ),
+
+            ),
+            SizedBox(height: 10),
+            Text(
+              '$growFlowerLevel',
+              style: TextStyle(
+                  color:Colors.amberAccent[200],
+                  fontFamily: 'IndieFlower',
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'rainjie87@gmail.com',
+                  style:TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1.0,
+                  ) ,
+
+
+                ),
+              ],
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
+
+
